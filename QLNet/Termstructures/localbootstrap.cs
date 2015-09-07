10ed135fd1261e71b8ab91abe992295728b01489
Andrea Maggiulli <a.maggiulli@gmail.com>
2015-09-07 15:56:14 +0200

    Update QLNet project URL.

--- QLNet/Termstructures/localbootstrap.cs
+++ QLNet/Termstructures/localbootstrap.cs
@@ -3,7 +3,7 @@
  Copyright (C) 2008-2014 Andrea Maggiulli (a.maggiulli@gmail.com)
  Copyright (C) 2014  Edem Dawui (edawui@gmail.com)
   
- This file is part of QLNet Project http://qlnet.sourceforge.net/
+ This file is part of QLNet Project https://github.com/amaggiulli/qlnet
 
  QLNet is free software: you can redistribute it and/or modify it
  under the terms of the QLNet license.  You should have received a
--- Test/T_OvernightIndexedSwap.cs
+++ Test/T_OvernightIndexedSwap.cs
@@ -1,12 +1,12 @@
 ﻿/*
  Copyright (C) 2008, 2009 , 2010  Andrea Maggiulli (a.maggiulli@gmail.com)
  * 
- This file is part of QLNet Project http://qlnet.sourceforge.net/
+ This file is part of QLNet Project https://github.com/amaggiulli/qlnet
 
  QLNet is free software: you can redistribute it and/or modify it
  under the terms of the QLNet license.  You should have received a
  copy of the license along with this program; if not, license is  
- available online at <http://qlnet.sourceforge.net/License.html>.
+ available online at <https://github.com/amaggiulli/qlnetLicense.html>.
   
  QLNet is a based on QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
