6bd5d5b83006422dc48980e886a795d40056aa9e
Andrea Maggiulli <a.maggiulli@gmail.com>
2016-03-24 16:08:07 +0100

    Added new Mountain Day holiday for Japan calendar.

--- QLNet/Time/Calendars/japan.cs
+++ QLNet/Time/Calendars/japan.cs
@@ -23,36 +23,37 @@ using System;
 
 namespace QLNet {
     //! Japanese calendar
-    /*! Holidays:
-        <ul>
-        <li>Saturdays</li>
-        <li>Sundays</li>
-        <li>New Year's Day, January 1st</li>
-        <li>Bank Holiday, January 2nd</li>
-        <li>Bank Holiday, January 3rd</li>
-        <li>Coming of Age Day, 2nd Monday in January</li>
-        <li>National Foundation Day, February 11th</li>
-        <li>Vernal Equinox</li>
-        <li>Greenery Day, April 29th</li>
-        <li>Constitution Memorial Day, May 3rd</li>
-        <li>Holiday for a Nation, May 4th</li>
-        <li>Children's Day, May 5th</li>
-        <li>Marine Day, 3rd Monday in July</li>
-        <li>Respect for the Aged Day, 3rd Monday in September</li>
-        <li>Autumnal Equinox</li>
-        <li>Health and Sports Day, 2nd Monday in October</li>
-        <li>National Culture Day, November 3rd</li>
-        <li>Labor Thanksgiving Day, November 23rd</li>
-        <li>Emperor's Birthday, December 23rd</li>
-        <li>Bank Holiday, December 31st</li>
-        <li>a few one-shot holidays</li>
-        </ul>
-        Holidays falling on a Sunday are observed on the Monday following
-        except for the bank holidays associated with the new year.
+   /*! Holidays:
+       <ul>
+       <li>Saturdays</li>
+       <li>Sundays</li>
+       <li>New Year's Day, January 1st</li>
+       <li>Bank Holiday, January 2nd</li>
+       <li>Bank Holiday, January 3rd</li>
+       <li>Coming of Age Day, 2nd Monday in January</li>
+       <li>National Foundation Day, February 11th</li>
+       <li>Vernal Equinox</li>
+       <li>Greenery Day, April 29th</li>
+       <li>Constitution Memorial Day, May 3rd</li>
+       <li>Holiday for a Nation, May 4th</li>
+       <li>Children's Day, May 5th</li>
+       <li>Marine Day, 3rd Monday in July</li>
+       <li>Mountain Day, August 11th (from 2016 onwards)</li>
+       <li>Respect for the Aged Day, 3rd Monday in September</li>
+       <li>Autumnal Equinox</li>
+       <li>Health and Sports Day, 2nd Monday in October</li>
+       <li>National Culture Day, November 3rd</li>
+       <li>Labor Thanksgiving Day, November 23rd</li>
+       <li>Emperor's Birthday, December 23rd</li>
+       <li>Bank Holiday, December 31st</li>
+       <li>a few one-shot holidays</li>
+       </ul>
+       Holidays falling on a Sunday are observed on the Monday following
+       except for the bank holidays associated with the new year.
 
-        \ingroup calendars
-    */
-    public class Japan : Calendar {
+       \ingroup calendars
+   */
+   public class Japan : Calendar {
         public Japan() : base(Impl.Singleton) { }
 
         class Impl : Calendar {
@@ -111,6 +112,8 @@ namespace QLNet {
                         && y >= 2003)
                     || ((d == 20 || (d == 21 && w == DayOfWeek.Monday)) && m == Month.July
                         && y >= 1996 && y < 2003)
+                    // Mountain Day (from 2016)
+                    || ( d == 11 && m == Month.August && y >= 2016 )
                     // Respect for the Aged Day (3rd Monday in September),
                     // was September 15th until 2003
                     || (w == DayOfWeek.Monday && (d >= 15 && d <= 21) && m == Month.September
