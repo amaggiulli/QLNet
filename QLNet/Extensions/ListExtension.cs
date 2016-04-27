3e9974b6b87b2ee0034e4bb2272ffeb2a8b24c97
Andrea Maggiulli <a.maggiulli@gmail.com>
2016-04-27 12:58:49 +0200

    Added FittedBondCurve example : Implemented B-spline basis functions, BernsteinPolynomial, FittedBondDiscountCurve, non linear fitting methods.

---
+++ QLNet/Extensions/ListExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QLNet
+{
+   public static class ListExtension
+   {
+      //    list: List<T> to resize
+      //    size: desired new size
+      //    element: default value to insert
+
+      public static void Resize<T>( this List<T> list, int size, T element = default(T) )
+      {
+         int count = list.Count;
+
+         if ( size < count )
+         {
+            list.RemoveRange( size, count - size );
+         }
+         else if ( size > count )
+         {
+            if ( size > list.Capacity )   // Optimization
+               list.Capacity = size;
+
+            list.AddRange( Enumerable.Repeat( element, size - count ) );
+         }
+      }
+   }
+}
