a95aa1c3e204fd52c96de9f1b33d74dc03aa8ddf
tournierjc <tournier.jc@gmail.com>
2018-10-17 14:12:15 +0200

    Update text format

--- src/QLNet/Settings.cs
+++ src/QLNet/Settings.cs
@@ -3,86 +3,114 @@
  Copyright (C) 2008 Toyin Akin (toyin_akin@hotmail.com)
  Copyright (C) 2008-2013 Andrea Maggiulli (a.maggiulli@gmail.com)
  Copyright (C) 2018 Jean-Camille Tournier (jean-camille.tournier@avivainvestors.com)
- 
+
  This file is part of QLNet Project https://github.com/amaggiulli/qlnet
 
  QLNet is free software: you can redistribute it and/or modify it
  under the terms of the QLNet license.  You should have received a
- copy of the license along with this program; if not, license is  
+ copy of the license along with this program; if not, license is
  available online at <http://qlnet.sourceforge.net/License.html>.
-  
+
  QLNet is a based on QuantLib, a free-software/open-source library
  for financial quantitative analysts and developers - http://quantlib.org/
  The QuantLib license is available online at http://quantlib.org/license.shtml.
- 
+
  This program is distributed in the hope that it will be useful, but WITHOUT
  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the license for more details.
 */
 using System;
 
-namespace QLNet {
-    // we need only one instance of the class
-    // we can not derive it from IObservable because the class is static
-    public static class Settings {
+namespace QLNet
+{
+   // we need only one instance of the class
+   // we can not derive it from IObservable because the class is static
+   public static class Settings
+   {
 
-        [ThreadStatic]
-        private static ObservableValue<Date> evaluationDate_;
+      [ThreadStatic]
+      private static ObservableValue<Date> evaluationDate_;
 
-        [ThreadStatic]
-        private static bool includeReferenceDateEvents_;
+      [ThreadStatic]
+      private static bool includeReferenceDateEvents_;
 
-        [ThreadStatic]
-        private static bool enforcesTodaysHistoricFixings_;
+      [ThreadStatic]
+      private static bool enforcesTodaysHistoricFixings_;
 
-        [ThreadStatic]
-        private static bool? includeTodaysCashFlows_;
+      [ThreadStatic]
+      private static bool? includeTodaysCashFlows_;
 
-        public static ObservableValue<Date> evaluationDate() 
-        {
-            if (evaluationDate_ == null)
-                evaluationDate_ = new ObservableValue<Date>(Date.Today);
-            return evaluationDate_; 
-        }
+      public static ObservableValue<Date> evaluationDate()
+      {
+         if (evaluationDate_ == null)
+            evaluationDate_ = new ObservableValue<Date>(Date.Today);
+         return evaluationDate_;
+      }
 
-        public static void setEvaluationDate(Date d) {
-            evaluationDate().Assign(d);
-            notifyObservers();
-        }
+      public static void setEvaluationDate(Date d)
+      {
+         evaluationDate().Assign(d);
+         notifyObservers();
+      }
 
-        public static bool enforcesTodaysHistoricFixings
-        {
-           get { return enforcesTodaysHistoricFixings_; }
-           set { enforcesTodaysHistoricFixings_ = value; }
-        }
+      public static bool enforcesTodaysHistoricFixings
+      {
+         get
+         {
+            return enforcesTodaysHistoricFixings_;
+         }
+         set
+         {
+            enforcesTodaysHistoricFixings_ = value;
+         }
+      }
 
-        public static bool includeReferenceDateEvents {
-            get { return includeReferenceDateEvents_; }
-            set { includeReferenceDateEvents_ = value; }
-        }
+      public static bool includeReferenceDateEvents
+      {
+         get
+         {
+            return includeReferenceDateEvents_;
+         }
+         set
+         {
+            includeReferenceDateEvents_ = value;
+         }
+      }
 
-        public static bool? includeTodaysCashFlows
-        {
-           get { return includeTodaysCashFlows_; }
-            set { includeTodaysCashFlows_ = value; }
-        }
+      public static bool? includeTodaysCashFlows
+      {
+         get
+         {
+            return includeTodaysCashFlows_;
+         }
+         set
+         {
+            includeTodaysCashFlows_ = value;
+         }
+      }
 
-        ////////////////////////////////////////////////////
-        // Observable interface
-        private static readonly WeakEventSource eventSource = new WeakEventSource();
-        public static event Callback notifyObserversEvent
-        {
-           add { eventSource.Subscribe(value); }
-           remove { eventSource.Unsubscribe(value); }
-        }
+      ////////////////////////////////////////////////////
+      // Observable interface
+      private static readonly WeakEventSource eventSource = new WeakEventSource();
+      public static event Callback notifyObserversEvent
+      {
+         add
+         {
+            eventSource.Subscribe(value);
+         }
+         remove
+         {
+            eventSource.Unsubscribe(value);
+         }
+      }
 
-        public static void registerWith(Callback handler) { notifyObserversEvent += handler; }
-        public static void unregisterWith(Callback handler) { notifyObserversEvent -= handler; }
-        private static void notifyObservers()
-        {
-           eventSource.Raise();
-        }
-    }
+      public static void registerWith(Callback handler) { notifyObserversEvent += handler; }
+      public static void unregisterWith(Callback handler) { notifyObserversEvent -= handler; }
+      private static void notifyObservers()
+      {
+         eventSource.Raise();
+      }
+   }
 
    // helper class to temporarily and safely change the settings
    public class SavedSettings : IDisposable
