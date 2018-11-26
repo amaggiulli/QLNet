c666aed5429ba71b420e370b9e49572a26d092cc
tournierjc <tournier.jc@gmail.com>
2018-11-26 10:11:15 +0100

    Update License

--- src/QLNet/Math/Optimization/GoldsteinLineSearch.cs
+++ src/QLNet/Math/Optimization/GoldsteinLineSearch.cs
@@ -8,7 +8,7 @@
 //
 //  QLNet is a based on QuantLib, a free-software/open-source library
 //  for financial quantitative analysts and developers - http://quantlib.org/
-//  The QuantLib license is available online at http://quantlib.org/license.shtml.
+//  The QuantLib license is available at <https://github.com/amaggiulli/QLNet/blob/develop/LICENSE>.
 //
 //  This program is distributed in the hope that it will be useful, but WITHOUT
 //  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
